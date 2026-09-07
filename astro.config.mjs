791bf1d1dda9096f995fcedcb9bc7c855d96078e
Loïc Frémont <lc.fremont@gmail.com>
2026-09-07 16:39:51 +0200

    Initial commit : site Astro Shotokan Karaté-dō Montfort + back office Decap

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [icon()],
+  server: {
+    allowedHosts: ['.trycloudflare.com'],
+  },
+  redirects: {
+    '/admin/': '/admin/index.html',
+    '/admin': '/admin/index.html',
+  }
+});
