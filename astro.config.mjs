6868b4bbdc6a48f77bb2134f306750bc984a60e7
Loïc Frémont <lc.fremont@gmail.com>
2026-09-08 10:38:01 +0200

    Restore GitHub Pages and remove CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skd-montfort-sur-meu.netlify.app',
+  site: 'https://skd-monfort-sur-meu.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
